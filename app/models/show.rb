74295cc207307449bb064363e88dd5a3a52bbcbf
Anthony Tilelli <anthony@tilelli.me>
2020-01-12 22:57:13 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    self.where("rating = ?", self.highest_rating).limit(1)
+    self.where("rating = ?", self.highest_rating).first
   end
   
   def self.lowest_rating
