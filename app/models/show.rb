480b3ab9b7d9e3884b18da6a1b3a52d1b82ca2ad
Anthony Tilelli <anthony@tilelli.me>
2020-01-12 22:44:16 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -19,7 +19,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.popular_shows
-    self.where("rating > ?", 3)
+    self.where("rating > ?", 5)
   end
 
   def self.shows_by_alphabetical_order
